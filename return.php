d96f39388d297b6213ab0a178a7a2362e95199a3
stevensiweix <stevensiweix@gmail.com>
2024-10-07 18:05:51 +0800

    調整說明

--- return.php
+++ return.php
@@ -11,17 +11,17 @@ $updateArray = json_decode($update, TRUE);
 
 if (isset($updateArray['message']['text'])) :
 
-    $telegram_token = $updateArray['message']['chat']['id'];
+    $telegram_token = $updateArray['message']['chat']['id']; // chatId
     $messageText    = $updateArray['message']['text'];
 
     $params = explode(" ", $messageText);
-    $telegram_token = $params[1]; // chatId
+    $code = $params[1]; //等於 $admin_info['code'] 可以更新至那位使用者
 
     if (!empty($telegram_token)):
 
         if (strpos($messageText, '/start') === 0):
 
-            // $telegram_token 存入資料庫
+            // $telegram_token 存入資料庫 更新 $admin_info['code'] 這個使用者
 
             // !這裡發通知，裡面我會夾帶 後台網址連結，將回到原本的後台頁面。
             telegram_notify_send($telegram_token, "系統回覆：Telegram 綁定成功。");
